e6a1c95b2a5856b121131292a8ea542f4eaa5389
OrangOlex <orangolex2850@proton.me>
2025-03-28 13:50:45 -0400

    make list look pretty :)

--- ChatBot.py
+++ ChatBot.py
@@ -3,7 +3,13 @@ greetings = ["Hello!", "Sup amigo?!", "Nice to meet you!"]
 goodbyes = ["Bye!", "Later Gator", "God Speed!", "See you on the other side."]
 
 keywords = [ "book", "game", "work", "party", "parties", "friend", ]
-responses = ["Books are a fun way to let your mind be enveloped by knowlage.", "Video games are so much fun to play but can get a little violent.", "Work sucks but it is nice to get paid.", "Parties are so much fun. I wish I could have been there.", "Parties are so much fun. I wish I could have been there.", "Friends are so much fun to be around and I hope we can be friends."]
+responses = ["Books are a fun way to let your mind be enveloped by knowlage.", 
+             "Video games are so much fun to play but can get a little violent.", 
+             "Work sucks but it is nice to get paid.", 
+             "Parties are so much fun. I wish I could have been there.", 
+             "Parties are so much fun. I wish I could have been there.", 
+             "Friends are so much fun to be around and I hope we can be friends."
+            ]
 
 print(random.choice(greetings) + " What did you do today?")
 
@@ -12,7 +18,7 @@ user = user.lower()
 
 while (user != "bye"):
     keyword_found = False
-    
+
     for index in range(len(keywords)):
         if (keywords[index] in user):
             print("Bot: " + responses[index])
